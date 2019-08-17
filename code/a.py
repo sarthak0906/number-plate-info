ecc59daa7f75b94b1e32a48fa1bf8ff0603689c7
sarthak khandelwal <sarthak0906@gmail.com>
2019-08-17 13:42:00 +0530

    Update a.py

--- code/a.py
+++ code/a.py
@@ -2,9 +2,9 @@ import numpy as np
 import pytesseract
 # pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 import cv2
-imag = cv2.imread("Test_01.png",1)
+imag = cv2.imread("Test_001.png",1)
 image = cv2.resize(imag,(500,400))
-image_gre = cv2.imread("Test_01.png",0)
+image_gre = cv2.imread("Test_001.png",0)
 image_grey = cv2.resize(image_gre,(500,400))    
 def imgshow(a):
     cv2.imshow('Test_011',a)
