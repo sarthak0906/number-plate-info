8deff8b024c67d0befc39f524145d065e0bffaeb
sarthak khandelwal <sarthak0906@gmail.com>
2019-09-13 22:27:25 +0530

    e

--- node/server.js
+++ node/server.js
@@ -49,67 +49,67 @@ app.get('/', (req, res) => {
 
 app.post('/predict', upload.single('test'), predict);
 
-// function predict(req, res){
-//     console.log('file', req.files)
-//     console.log('body', req.body)
+function predict(req, res){
+    console.log('file', req.files)
+    console.log('body', req.body)
 
-//     const exec = require('child_process').exec;
-//     exec('predict.bat', (err, stdout, stderr) => {
-//         if (err){
-//             console.error(err);
-//             res.send('error')
-//             return;
-//         }
-        
-//         if (stderr){
-//             console.error(stderr);
-//             res.send('error')
-//             return;
-//         }
-
-//         res.send(stdout);
-//     })
-// }
-
-function predict (req, res){
-    var result = "";
-    // res.status(200).json({
-    //     message: 'success!',
-    // })
-
-    amqp.connect('amqp://localhost', (err, con) => {
+    const exec = require('child_process').exec;
+    exec('predict.bat', (err, stdout, stderr) => {
         if (err){
-            return console.log(err);
+            console.error(err);
+            res.send('error')
+            return;
         }
         
-        console.log('amqp connected');
-        con.createChannel((err, ch) => {
-            if (err){
-                return console.log(err);
-            }
-            console.log('channel created');
-            var simulations = 'simulations';
-            ch.assertQueue(simulations, {durable: false});
-            
-            var results = 'results';
-            ch.assertQueue(results, {durable: false});
-            
-            ch.sendToQueue(simulations, new Buffer(JSON.stringify("something")));
-            
-            ch.consume(results, (msg) => {
-                console.log('result sent');
-                result = msg.content.toString();
-                console.log('result is ' + result);
-                res.end(JSON.stringify({'result': result}));
-                // res.send(JSON.stringify({'result': result}));
-                // return res.send()
-            });
-            // setTimeout(() => {con.close()}, 500);
-        })
-    })
+        if (stderr){
+            console.error(stderr);
+            res.send('error')
+            return;
+        }
 
-    // res.send(result);
+        res.send(stdout);
+    })
 }
+
+// function predict (req, res){
+//     var result = "";
+//     // res.status(200).json({
+//     //     message: 'success!',
+//     // })
+
+//     amqp.connect('amqp://localhost', (err, con) => {
+//         if (err){
+//             return console.log(err);
+//         }
+        
+//         console.log('amqp connected');
+//         con.createChannel((err, ch) => {
+//             if (err){
+//                 return console.log(err);
+//             }
+//             console.log('channel created');
+//             var simulations = 'simulations';
+//             ch.assertQueue(simulations, {durable: false});
+            
+//             var results = 'results';
+//             ch.assertQueue(results, {durable: false});
+            
+//             ch.sendToQueue(simulations, new Buffer(JSON.stringify("something")));
+            
+//             ch.consume(results, (msg) => {
+//                 console.log('result sent');
+//                 result = msg.content.toString();
+//                 console.log('result is ' + result);
+//                 res.end(JSON.stringify({'result': result}));
+//                 // res.send(JSON.stringify({'result': result}));
+//                 // return res.send()
+//             });
+//             // setTimeout(() => {con.close()}, 500);
+//         })
+//     })
+
+//     // res.send(result);
+// }
 
 app.listen(8000, async () => {    
     console.log('server running on port 8000');
