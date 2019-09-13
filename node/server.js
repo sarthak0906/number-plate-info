d4162ec4c284ddbaf1d0a055e2f37c26324242c4
sarthak khandelwal <sarthak0906@gmail.com>
2019-09-13 22:57:54 +0530

    Update server.js

--- node/server.js
+++ node/server.js
@@ -67,7 +67,7 @@ function predict(req, res){
             return;
         }
 
-        res.send(JSON.stringify({"result": result}));
+        res.send(JSON.stringify({"result": stdout}));
     })
 }
 
