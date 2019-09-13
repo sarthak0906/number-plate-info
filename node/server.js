cb5b2348e325f7eb6483a6027515383756720066
sarthak khandelwal <sarthak0906@gmail.com>
2019-09-13 20:37:54 +0530

    Update server.js

--- node/server.js
+++ node/server.js
@@ -111,6 +111,6 @@ function predict (req, res){
     // res.send(result);
 }
 
-app.listen(8000, async () => {    
+app.listen(8080, async () => {    
     console.log('server running on port 8000');
 })
